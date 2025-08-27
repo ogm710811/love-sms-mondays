d91e632ef2cf2f41a691a7e69c88e6585c19a553
Orestes Garcia <ogm710811@gmail.com>
2025-08-26 23:10:42 -0400

    Update AWS region in test configuration and add DependsOn to Lambda template
    Changed the default AWS region in `conftest.py` to `us-east-1` for consistency. Added `DependsOn` attribute to the Lambda function `LogGroup` in the CloudFormation template to ensure proper resource initialization sequencing.

--- tests/conftest.py
+++ tests/conftest.py
@@ -15,4 +15,4 @@ def aws_credentials(monkeypatch):
     monkeypatch.setenv("AWS_ACCESS_KEY_ID", "testing")
     monkeypatch.setenv("AWS_SECRET_ACCESS_KEY", "testing")
     monkeypatch.setenv("AWS_SESSION_TOKEN", "testing")
-    monkeypatch.setenv("AWS_DEFAULT_REGION", "us-east-2")
+    monkeypatch.setenv("AWS_DEFAULT_REGION", "us-east-1")
